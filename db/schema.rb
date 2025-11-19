d808c24aac7f031da76fb15450e547fc2b2ff6ee
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2025-11-19 21:26:45 +0000

    add sub model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_13_095936) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_19_212600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,4 +25,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_13_095936) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "web_push_subscriptions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "endpoint"
+    t.string "p256dh_key"
+    t.string "auth_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_web_push_subscriptions_on_user_id"
+  end
+
+  add_foreign_key "web_push_subscriptions", "users"
 end
