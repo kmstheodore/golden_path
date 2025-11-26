fda8f737a1609609ef333141bd63f4181743a09e
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2025-11-26 14:22:32 +0000

    .

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     if current_user.web_push_subscriptions.empty?
       redirect_to new_web_push_subscription_path
     end
+    @paths = current_user.paths.order(strike_time: :asc)
   end
 
   def welcome
